d20fcc6f8ec2892d878c58cdc4532bc6fcd84a83
Krzycholl18 <32575999+krzychsol@users.noreply.github.com>
2023-03-24 18:37:28 +0100

    lab3 - in progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from modules import shape
 
 if __name__ == '__main__':
-    sh = shape.Shape(2,5,7)
-    sph = shape.Sphere(sh)
-    print(sh.__str__())
+    #:TODO Przetestować działanie klasy shape
+    # Dodać potrzebne pola i metody
+    
