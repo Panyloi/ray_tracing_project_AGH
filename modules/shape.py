263d9fea6916169f9fedde3443f834cff0d2ea14
Panyloi <kocdecke@gmail.com>
2023-03-26 21:25:12 +0200

    change some things

--- modules/shape.py
+++ modules/shape.py
@@ -1,47 +1,59 @@
 import math
-from PIL import Image
+# from PIL import Image
+from modules.vec3 import Vec3
+from modules.imageSaver import Image
 
 class Shape:
     def __init__(self, x=0, y=0, z=0):
-        self.x = x
-        self.y = y
-        self.z = z
+        # self.x = x
+        # self.y = y
+        # self.z = z
+        self.position = Vec3(x, y, z)
         
     def __str__(self):
         return f"({self.x}, {self.y}, {self.z})"
         
-    def translate(self, dx, dy, dz):
-        self.x += dx
-        self.y += dy
-        self.z += dz
+    # def translate(self, dx, dy, dz):
+    def translate(self, other : Vec3):
+        # self.x += dx
+        # self.y += dy
+        # self.z += dz
+        self.position += other
         
     def save_to_ppm(self, filename):
-        size = (100, 100) # size of the image
-        im = Image.new('RGB', size, (255, 255, 255)) # create a white image
+        size = (1000, 1000) # size of the image
+        # im = Image.new('RGB', size, (255, 255, 255)) # create a white image
+        im = Image(image_width=size[0], image_height=size[1]) # create a white image
+        # pixel = im.load() # create the pixel map
         pixel = im.load() # create the pixel map
 
         # iterate over the image and color the pixels according to the shape
         for i in range(size[0]):
             for j in range(size[1]):
                 if self.contains(i, j):
-                    pixel[i, j] = (0, 0, 0) # set the pixel to black
+                    # pixel[i, j] = (0, 0, 0) # set the pixel to black
+                    pixel[i][j] = Vec3(0, 0, 0) # set the pixel to black
 
         # save the image to file
-        im.save(filename, "PPM")
+        # im.save(filename, "PPM")
+        im.save(filename, pixel)
 
-    def save_to_png(self, filename):
-        size = (100, 100) # size of the image
-        im = Image.new('RGB', size, (255, 255, 255)) # create a white image
-        pixel = im.load() # create the pixel map
+    # TODO maybe later implement PNG format :"DDD
 
-        # iterate over the image and color the pixels according to the shape
-        for i in range(size[0]):
-            for j in range(size[1]):
-                if self.contains(i, j):
-                    pixel[i, j] = (0, 0, 0) # set the pixel to black
+    # def save_to_png(self, filename):
+    #     size = (100, 100) # size of the image
+    #     im = Image.new('RGB', size, (255, 255, 255)) # create a white image
+    #     pixel = im.load() # create the pixel map
 
-        # save the image to file
-        im.save(filename, "PNG")
+    #     # iterate over the image and color the pixels according to the shape
+    #     for i in range(size[0]):
+    #         for j in range(size[1]):
+    #             if self.contains(i, j):
+    #                 pixel[i, j] = (0, 0, 0) # set the pixel to black
+
+    #     # save the image to file
+    #     im.save(filename, "PNG")
+    
 
 class Sphere(Shape):
     def __init__(self, x=0, y=0, z=0, radius=1):
@@ -50,10 +62,14 @@ class Sphere(Shape):
         
     def contains(self, x, y):
         # calculate the distance between the point and the center of the sphere
-        distance = math.sqrt((x - self.x)**2 + (y - self.y)**2 + (self.z)**2)
+        distance = math.sqrt((x - self.position.x)**2 + (y - self.position.y)**2 + (self.position.z)**2)
         
         # check if the point is inside the sphere
         return distance <= self.radius
+
+    # get center of sphere
+    def center(self):
+        return self.position
 
 class Cuboid(Shape):
     def __init__(self, x=0, y=0, z=0, width=1, height=1, depth=1):
