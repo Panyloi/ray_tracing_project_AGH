5e9ac175959c057b791a70dd718d460ac234c206
Panyloi <kocdecke@gmail.com>
2023-03-26 21:24:03 +0200

    New testing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from modules import shape
 
+
 if __name__ == '__main__':
     #:TODO Przetestować działanie klasy shape
     # Dodać potrzebne pola i metody
-    
+    sphere1 = shape.Sphere(500, 500, 40, radius=100)
+    sphere1.save_to_ppm("sphere1.ppm")
