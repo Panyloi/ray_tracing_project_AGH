16b217cc3bfbc41ddab4f8cccb10fe7d0b313860
Panyloi <kocdecke@gmail.com>
2023-04-24 03:46:21 +0200

    Little changing

--- modules/constants.py
+++ modules/constants.py
@@ -1,6 +1,11 @@
 from modules.vec3 import Vec3
+
 # library for holding all variables that are used in project
 BLACK = Vec3(0, 0, 0)
 WHITE = Vec3(255, 255, 255)
-WIDTH = 1000
-HEIGHT = 1000
+ASPECT_RATIO = 16.0 / 9.0
+WIDTH = 400
+HEIGHT = int(WIDTH / ASPECT_RATIO)
+VW = 1  # viewport width
+VH = 1  # viewport height
+DISTANCE = 1  # distance between canvas and viewport
