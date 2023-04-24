14466e78c82a139dbea5825ca6aab73864c98dac
Panyloi <kocdecke@gmail.com>
2023-04-24 10:15:45 +0200

    Adding library for light

---
+++ modules/light.py
@@ -0,0 +1,41 @@
+from modules.vec3 import Vec3
+
+point3 = Vec3
+color = Vec3
+
+class Light:
+    def __init__(self, type : str, intensity : float = 0.0, direction : Vec3 = Vec3(), position : point3 = point3()) -> None:
+        self.type = type
+        self.intensity = intensity
+        self.direction = direction
+        self.position = position
+
+class LightList:
+    def __init__(self, object = None) -> None:
+        self.lights = list() if object is None else list(object)
+    
+    def clear(self):
+        self.lights.clear()
+    
+    def add(self, object):
+        self.lights.append(object)
+    
+    # P - point where ligth and shape intersect
+    # N - normal vector from center of shape to P
+    def compute_lighting(self, P: point3, N: point3):
+        i = 0.0
+
+        for light in self.lights:
+            if light.type == "ambient":
+                i += light.intensity
+            else:
+                if light.type == "point":
+                    L = light.position - P
+                else:
+                    L = light.direction
+                
+                n_dot_l = L.dot(N)
+                if n_dot_l > 0:
+                    i += light.intensity * n_dot_l/(N.length() * L.length())
+        
+        return i
