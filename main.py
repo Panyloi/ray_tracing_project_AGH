a45ac18554fdd85bb155fd71371dea7de6e1824c
Krzycholl18 <32575999+krzychsol@users.noreply.github.com>
2023-05-21 11:14:16 +0200

    Some refactor

--- main.py
+++ main.py
@@ -8,15 +8,14 @@ from modules.vec3 import Vec3
 from modules.ray import Ray
 from modules.light import LightList, Light
 
-
 # aliasing
 color = Vec3
 point3 = Vec3
 
 
 # setting background
-def reflect_ray(R:Vec3,N:Vec3):
-    return N*2*N.dot(R)-R
+def reflect_ray(R: Vec3, N: Vec3):
+    return N * 2 * N.dot(R) - R
 
 
 def ray_color(r: Ray, world):
@@ -30,7 +29,7 @@ def ray_color(r: Ray, world):
     return color(1.0, 1.0, 1.0) * (1 - t) + color(0.5, 0.7, 1.0) * t
 
 
-def trace_ray(r: Ray, world, light: LightList, recursion_depth = 3):
+def trace_ray(r: Ray, world, light: LightList, recursion_depth=3):
     rec = HitRecord()
     is_world_hit, rec, closest_sphere, closest_t = world.hit(r, 0.001, float("inf"), rec)
     if closest_sphere is None:
@@ -38,48 +37,40 @@ def trace_ray(r: Ray, world, light: LightList, recursion_depth = 3):
         # t = 0.5 * (unit_direction.y + 1.0)
         # return color(1.0, 1.0, 1.0) * (1 - t) + color(0.5, 0.7, 1.0) * t
         return BLACK
-    
-    #Compute local color
-    P = r.orig+r.dir*closest_t
+
+    # Compute local color
+    P = r.orig + r.dir * closest_t
     N = P - closest_sphere.center
     N = N.normalized()
-    local_color = closest_sphere.sphere_color * light.compute_lighting(P, N, world, r.dir*(-1), closest_sphere.specular)
-    #return closest_sphere.sphere_color * light.compute_lighting(rec.p, rec.normal, world)
-    
-    #If we hit the recursion limit or the object is not reflective, we're done
+    local_color = closest_sphere.sphere_color * light.compute_lighting(P, N, world, r.dir * (-1),
+                                                                       closest_sphere.specular)
+    # return closest_sphere.sphere_color * light.compute_lighting(rec.p, rec.normal, world)
+
+    # If we hit the recursion limit or the object is not reflective, we're done
     ref = closest_sphere.reflective
     if recursion_depth <= 0 or ref <= 0:
         return local_color
-    
-    #Compute the reflected color
-    R = reflect_ray(r.dir*(-1),N)
+
+    # Compute the reflected color
+    R = reflect_ray(r.dir * (-1), N)
     r.orig = P
     r.dir = R
-    reflected_color = trace_ray(r,world, light, recursion_depth-1)
-    
-    return local_color*(1-ref)+reflected_color*ref
+    reflected_color = trace_ray(r, world, light, recursion_depth - 1)
+
+    return local_color * (1 - ref) + reflected_color * ref
+
 
 if __name__ == "__main__":
-    #:TODO Przetestować działanie klasy shape
-    # Dodać potrzebne pola i metody
-    # image = Image()
-    # sphere1 = shape.Sphere(500, 500, 40, radius=100).updatePixelMap(image)
-    # sphere2 = shape.Sphere(250, 250, 40, radius=100).updatePixelMap(image, color=Vec3(255, 0, 255))
-    # cuboid1 = shape.Cuboid(750, 750, 0, 50, 50).updatePixelMap(image)
-
     # image
     image = Image()
 
     # world
     world = HittableList()
-    # world.add(Sphere(point3(0,0,-1), 0.5))
-    # world.add(Sphere(point3(0,-100.5,-1), 100))
-    world.add(Sphere(point3(0, -1, -3), 1, sphere_color= color(1, 0, 0), reflective=0.3, specular=1000))
-    world.add(Sphere(point3(2, 0, -4), 1, sphere_color = color(0, 0, 1), reflective=0.7, specular= 300))
-    world.add(Sphere(point3(-2, 0, -4), 1, sphere_color = color(0, 1, 0), reflective=0.1, specular= 100))
+    world.add(Sphere(point3(0, -1, -3), 1, sphere_color=color(1, 0, 0), reflective=0.3, specular=1000))
+    world.add(Sphere(point3(2, 0, -4), 1, sphere_color=color(0, 0, 1), reflective=0.7, specular=300))
+    world.add(Sphere(point3(-2, 0, -4), 1, sphere_color=color(0, 1, 0), reflective=0.1, specular=100))
     # ground
     world.add(Sphere(point3(0, -5001, 0), 5000, sphere_color=color(1, 1, 0)))
-    
 
     # light
     light = LightList()
@@ -95,22 +86,17 @@ if __name__ == "__main__":
     origin = point3(0, 0, 0)
     horizontal = Vec3(viewport_width, 0, 0)
     vertical = Vec3(0, viewport_heigh, 0)
-    lower_left_corner = origin - horizontal/2 - vertical/2 - Vec3(0, 0, focal_length)
+    lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vec3(0, 0, focal_length)
 
     # render
     for j in range(HEIGHT - 1, -1, -1):
-        # print("Scaning remining: {}\r".format(j))
+        print("Scaning remining: {}\r".format(j))
         for i in range(WIDTH):
             u = i / (WIDTH - 1)
             v = j / (HEIGHT - 1)
             r = Ray(origin, lower_left_corner + horizontal * u + vertical * v - origin)
-            # pixel_color = ray_color(r, world)
-            pixel_color = trace_ray(r, world, light)
+            pixel_color = ray_color(r, world)
+            # pixel_color = trace_ray(r, world, light)
             image.write_color((j, i), pixel_color)  # some modulo staff
 
-
-    # cuboid2 = shape.Cuboid(900, 900, 0, 150, 100).updatePixelMap(image, color=Vec3(128, 255, 0))
-    image.save("listing2.ppm")
-
-# UWAGI
-# przy mnożeniu przez sklara Vec3 -> scalar musi być po prawej stronie bo python krzyczy błąd TypeError: unsupported operand type(s) for *: 'float' and 'Vec3'
+    image.save("test.ppm")
--- modules/imageSaver.py
+++ modules/imageSaver.py
@@ -1,17 +1,17 @@
 from modules.vec3 import Vec3
 from modules.constants import *
 
-class Image: # resposnisble for creating and managing image
-    def __init__(self, filename = None, mcv = 255) -> None:
+
+class Image:  # resposnisble for creating and managing image
+    def __init__(self, filename=None, mcv=255) -> None:
         # self.image_width = image_width
         # self.image_height = image_height
         self.filename = filename
-        self.mcv = mcv # maximum color value
+        self.mcv = mcv  # maximum color value
         self.pixelmap = [[WHITE for _ in range(WIDTH)] for _ in range(HEIGHT)]
         self.shapes = []
 
-
-    def save(self, filename): # opening file to whole class
+    def save(self, filename):  # opening file to whole class
         self.f = filename
         with open(filename, "w") as self.f:
             self.init_file()
@@ -21,17 +21,14 @@ class Image: # resposnisble for creating and managing image
         # return image all white -> prepered to draw
         return self.pixelmap
 
-
-    def init_file(self): # initializate ppm file -> header, width, height and maximum color value
+    def init_file(self):  # initializate ppm file -> header, width, height and maximum color value
         self.f.write("P3\n")
         self.f.write(str(WIDTH) + ' ' + str(HEIGHT) + '\n' + str(self.mcv) + '\n')
-    
 
-
-    def create_image(self): # method for creating ppm image 
+    def create_image(self):  # method for creating ppm image
         for j in range(HEIGHT - 1, -1, -1):
             for i in range(WIDTH):
-                pixel =  (self.pixelmap[j][i]).int()
+                pixel = (self.pixelmap[j][i]).int()
 
                 self.f.write(str(pixel.x) + ' ' + str(pixel.y) + ' ' + str(pixel.z) + '\n')
 
@@ -39,12 +36,13 @@ class Image: # resposnisble for creating and managing image
     def update_map(self, add_points: set, delete_points: set, color: Vec3):
         for i, j in add_points:
             self.pixelmap[i][j] = color
-        
+
         for i, j in delete_points:
             self.pixelmap[i][j] = WHITE
 
-    def write_color(self, cords : tuple[int, int], pixel_color: Vec3):
-        self.pixelmap[cords[0]][cords[1]] = Vec3(255.999 * pixel_color.x, 255.999 * pixel_color.y, 255.999 * pixel_color.z).int()
+    def write_color(self, cords: tuple[int, int], pixel_color: Vec3):
+        self.pixelmap[cords[0]][cords[1]] = Vec3(255.999 * pixel_color.x, 255.999 * pixel_color.y,255.999 * pixel_color.z).int()
+
 
 def image_to_viewport(pixel_x, pixel_y):
     return Vec3(pixel_x * VW / WIDTH, pixel_y * VH / HEIGHT, DISTANCE)
